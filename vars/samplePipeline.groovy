d93010e74c3a99cf08e76549005986f262387b2e
Kirubakaran Kuppusamy <scmmeeting@gmail.com>
2020-11-11 12:34:51 +0000

    Update samplePipeline.groovy
    update with sample pipeline

--- vars/samplePipeline.groovy
+++ vars/samplePipeline.groovy
@@ -1,13 +1,39 @@
-def call() {
-    node {     
-        stage('SCM') {
-            sh 'echo git checkout'
-        }        
-        stage('Build & Test') {
-            sh 'echo build and test'
-        }        
-        stage('Deploy') {
-                sh 'echo deploy'
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
+                }
+            }
+
+            stage('build') {
+                steps {
+                    sh 'mvn clean package -DskipTests=true'
+                }
+            }
+
+            stage ('test') {
+                steps {
+                    parallel (
+                        "unit tests": { sh 'mvn test' },
+                        "integration tests": { sh 'mvn integration-test' }
+                    )
+                }
+            }
+
+            stage('deploy'){
+                steps {
+                    sh 'echo deploy'
+                }
+            }
+
+        post {
+            failure {
+                mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
+            }
         }
     }
 }
